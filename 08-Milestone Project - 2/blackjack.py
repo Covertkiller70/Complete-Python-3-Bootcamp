0ad83b73b0dc2068fa9c6bb4a34073b404a5f3d7
Frank Sarnelli <sarnelli@C02WGLX2J1WK-Frank-Sarnelli.local>
2018-09-03 08:22:21 -0500

    almost done

--- 08-Milestone Project - 2/blackjack.py
+++ 08-Milestone Project - 2/blackjack.py
@@ -37,37 +37,49 @@ class Player:
         self.name = input("Enter your playername: ")
         self.account = 100
         self.bet = 0
+        self.push = False
         self.stillin = True
-        self.soft = False
         self.split= False
     
-    def __str__(self):
-        return self.name + " has $" + str(self.account) + " left" 
+    #def __str__(self):
+    #    return self.name + " has $" + str(self.account - self.bet) + " left" 
  
     def sethand(self,hand):
         self.hand = hand
-        print(f"{self.name} was delt a [{self.hand[0]}] and a [{self.hand[1]}]")
-    
+
+    def showhand(self):
+        handtxt = ''
+        for card in self.hand:
+            handtxt += f"[{card.value} of {card.suite}] "
+        return handtxt
+
     def win(self,chips):
         self.account += chips
+        print(f"{self.name} Account balance: ${self.account}")
     
     def lose(self,chips):
-        if self.account - chips < 0:
-            self.enabled = False
-            return (f"Player {self.name} is out of the game!")
+        if self.account - chips < 10:
+            self.stillin = False
+            print(f"Player {self.name} is out of the game!")
         else:
             self.account -= chips
+            print(f"{self.name} Account balance: ${self.account}")
 
 class Dealer:
     def __init__(self):
         self.account = 10000000
         self.soft = False
         self.blackjack = False
+    def __str__(self):
+        print(f"Dealer has [{self.hand[0]}] and a [{self.hand[1]}]")
     def sethand(self,hand):
         self.hand = hand
         print(f"Dealer has [{self.hand[0]}], and one unknown")
     def showhand(self):
-        print(f"Dealer has [{self.hand[0]}] and a [{self.hand[1]}]")
+        handtxt = ''
+        for card in self.hand:
+            handtxt += f"[{card.value} of {card.suite}] "
+        return handtxt
 
 # === Methods and functionss ===
 # Gets number of players playing this round
@@ -89,10 +101,11 @@ def placebet(player):
         try:
             bet = int(input(f"{player.name} enter a number to bet: "))
             if (player.account - bet) > 0:
-                player.account -= bet
                 player.bet = bet
-                print(f"{player.name} bets ${str(bet)}, ${str(player.account)} left")
+                print(f"{player.name} bets ${str(bet)}, ${str(player.account - player.bet)} left\n")
                 break
+            elif bet < 10:
+                print("$10 minimum bets! Try again with a higher bet...")
             else:
                 print(f"Bet must be less than ${str(player.account)}!")
         except:
@@ -101,16 +114,22 @@ def placebet(player):
 # Figures out the hand value and returns total
 def gethandvalue(player, hand):
     total = 0
+    # gets the total of the hand as is
     for card in hand:
-        if card.value == "Ace":
-            player.soft == True
-        if card.value in ("Ace", "Jack","Queen","King"):
+        if card.value in ("Jack","Queen","King"):
             total += 10
+        elif card.value == "Ace":
+            total += 11
         else:
             total += int(card.value)
+    # If the hand has Aces and is over 21, change the Aces to 1
+    for card in hand:
+        if card.value == "Ace" and total > 21:
+            total -= 10
     return total
 
 # Checks the given cards, sees if a condition is met
+# Need to add insurance
 def checkcards(players,dealer,first):
     dtotal = gethandvalue(dealer, dealer.hand)
     for player in players:
@@ -125,49 +144,79 @@ def checkcards(players,dealer,first):
                     players[player].lose(players[player].bet)
                     print(f"{players[player].name} lost ${players[player].bet}")   
             if first == False:
-                if total > dtotal or (total == 21 and dtotal != 21):
-                    players[player].win(players[player].bet * 2)
-                    print(f"{players[player].name} won ${players[player].bet * 2}")
-                elif dtotal > total:
-                    players[player].lose(players[player].bet)
+                if (dtotal > total and dtotal < 21) or total > 21 or (dtotal == 21 and total != 21):
                     print(f"{players[player].name} lost ${players[player].bet}")
+                    players[player].lose(players[player].bet)
+                    players[player].push = False
+                elif (21 > total > dtotal) or (21 > total and dtotal > 21) or (total == 21 and dtotal != 21):
+                    print(f"{players[player].name} won ${players[player].bet * 2}")
+                    players[player].win(players[player].bet * 2)
+                    players[player].push = False
+                elif dtotal == total:
+                    print(f"{players[player].name} pushes")
+                    players[player].push = True
 
 # Incites action for player for their turn
-# need to add in soft hits
+# Need to account for splits 
 def playeraction(players,thedeck):
     for player in players:
-        while True:     
+        while True:
+            print(f"{players[player].name} has {players[player].showhand()}, count is {gethandvalue(players[player], players[player].hand)}")     
             action = input(f"{players[player].name}, hit or stay?: ")
-            if action == "Hit" or action == "hit":
+            if action in ("HIT", "hit", "Hit", "1","H","h"):
                 players[player].hand.append(thedeck.dealcard())
-                print(f"{players[player].name} pulls a {players[player].hand[-1]}")
                 total = gethandvalue(players[player], players[player].hand)
+                print(f"\n{players[player].name} pulls a {players[player].hand[-1]}, count is {total}")
                 if total > 21:
-                    players[player].stillin = False
-                    print(f"{players[player].name} busts at {total}")
+                    print(f"{players[player].name} busts at {total}\n")
                     break
-                else:
-                    print(f"count is {total}")
-            elif action == "Stay" or action == "stay":
-                print(f"{players[player].name} stays at {gethandvalue(players[player], players[player].hand)}")
+                elif total == 21:
+                    print(f"{players[player].name} got a Blackjack!\n")
+                    break
+            elif action in ("stay","STAY","Stay","0","s","S"):
+                print(f"{players[player].name} stays at {gethandvalue(players[player], players[player].hand)}\n")
                 break
+
+# Dealers actions 
+def dealeraction(dealer, thedeck):
+    while True:
+        print(f"Dealer has {dealer.showhand()}, count is {gethandvalue(dealer, dealer.hand)}")
+        total = gethandvalue(dealer, dealer.hand)
+        if total > 21:
+            print(f"Dealer busts at {total}")
+            break
+        elif total == 21:
+            print(f"Dealer gets Blackjack!")
+            break
+        elif total <= 16:
+            print("Dealer hits...")
+            dealer.hand.append(thedeck.dealcard())
+        elif total >= 17:
+            print(f"Dealer stays, count is {total}")
+            break
+
+# Check for players remaining
+def checkremainingplayers(players):
+    return players
 
 # =========== Main code running =============
 # ===========================================
 # Intro message, deck setup and dealer creation
+players = getplayers()
 thedeck = Deck()
 thedeck.shuffle()
-players = getplayers()
 dealer = Dealer()
-for player in players:
-    placebet(players[player])
-    players[player].sethand(thedeck.dealhand())
-dealer.sethand(thedeck.dealhand())
-# Check if anyone got a blackjack
-checkcards(players,dealer,first=True)
-playeraction(players, thedeck)
-# dealer shows second card
-# dealer starts hitting/staying
-# hits on 16 stays on 17, hits on a soft 17 
-# Compare cards again
-# Pay out winners, take from losers
+while True:        
+    for player in players:
+        if players[player].push == False:
+            placebet(players[player])
+        players[player].sethand(thedeck.dealhand())
+    dealer.sethand(thedeck.dealhand())
+    checkcards(players,dealer,first=True)
+    playeraction(players, thedeck)
+    dealeraction(dealer,thedeck)
+    checkcards(players,dealer,first=False)
+
+    choice = input("Do you want to play again? (y/n): ")
+    if choice in ("n","N"):
+        break
